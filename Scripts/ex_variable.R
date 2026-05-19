d348700233e6639f57432a48c23ac216f323b13c
Mohammmadabduleee <abdul.eee.ctg@gmail.com>
2026-05-19 12:15:14 -0700

    Add data structure

---
+++ Scripts/ex_variable.R
@@ -0,0 +1,12 @@
+# This is a comment
+num_1 <-10
+num_2 <-30
+addition<- num_1+num_2
+addition
+#Variables or Assignment
+My_age <-89
+class(My_age)
+name <-"Abdul Kader"
+class(name)
+is_weekend <-T
+class(is_weekend)
